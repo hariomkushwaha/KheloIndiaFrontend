d21241fc3482e40ab8ca134ed487ac19417a429d
Shubh Agrawal <shubhagrawal1280@gmail.com>
2022-06-28 19:57:51 +0530

    adding template : inc

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ProponentProvider } from "./context/ProponentContext";
 import { AdminProvider } from "./context/AdminContext";
-
 import { ChakraProvider } from "@chakra-ui/react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./components/dashboard/layouts/Auth";
+
+import AdminLayout from "./components/dashboard/layouts/Admin";
+import RTLLayout from "./components/dashboard/layouts/RTL";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/auth" element={<AuthLayout />} />
+          <Route path="/admin" element={<AdminLayout />} />
+          <Route path="/rtl" element={<RTLLayout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <ProponentProvider>
       <AdminProvider>
         <ChakraProvider>
