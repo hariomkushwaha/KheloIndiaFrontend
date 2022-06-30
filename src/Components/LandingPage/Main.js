255a01814ce9e7fb059b91fdba88e4a96561c341
Navya <navyagoyal400@gmail.com>
2022-06-30 23:16:38 +0530

    landing page

---
+++ src/Components/LandingPage/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Header from "./Header";
+import Hero from './Hero';
+import About from './About';
+import Footer from './Footer';
+
+const Main = () => {
+  return (
+    <div>
+    <Header />
+    <Navbar />
+    <Hero />
+    <About />
+    <Footer />
+    </div>
+  )
+}
+
+export default Main;
