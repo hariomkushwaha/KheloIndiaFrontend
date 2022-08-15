7ff04d043442b3581dafb82c576a0b8f3c7a3a68
navya1808 <navyagoyal400@gmail.com>
2022-08-15 17:53:31 +0530

    charts

--- src/pages/Chart.js
+++ src/pages/Chart.js
@@ -1,36 +1,69 @@
-import React from "react";
+import React , {useEffect, useState} from "react";
 import BarChart from "../components/BarChart.jsx";
 import LineChart from "../components/LineChart.jsx";
 import PieChart from "../components/PieChart.jsx";
 import  {faker} from "@faker-js/faker";
 import Header from "../components/Header.jsx";
+import { dateFnsLocalizer } from "react-big-calendar";
 
-import { faker } from "@faker-js/faker";
-import { faker } from "@faker-js/faker";
-import  {faker} from "@faker-js/faker";
-import Header from "../components/Header.jsx";
+
 
 const Chart = () => {
-  const labels = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July"
-  ];
+//   const [proponentValues, setProponentValues] = useState([]);
+//   const [arr , setArr] = useState([]);
+// const handleProponents = async () => {
+//   try {
+//     const res = await fetch("/API/proponentform", {
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//       },
+//       credentials: "include",
+//     });
+//     const data = res.json();
+//     data.then((response) => {
+//       setProponentValues(response);
+//       console.log(response);
+//     });
+//     if (!res.status === 200) {
+//       const error = new Error(res.error);
+//       throw error;
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: "Dataset 1",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-    ],
-  };
+// useEffect(() => {
+//   handleProponents();
+// }, []);
+
+// useEffect(()=>{
+//   console.log(arr);
+// },[arr]);
+
+//   const labels = [
+//     "durability",
+//     "quality",
+//     "usability"
+//   ];
+
+//   const data = {
+//     labels,
+//     datasets: [
+//       {
+//         label: "Dataset 1",
+//         data: labels.forEach((num)=>{
+             
+//              console.log(proponentValues);
+//              setArr( ar => [...ar, proponentValues[0].num]);
+             
+//         }),
+//         backgroundColor: "rgba(255, 99, 132, 0.5)",
+//       },
+//     ],
+//   };
 
   return (
     <>
