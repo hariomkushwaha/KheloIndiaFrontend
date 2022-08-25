106dbc128aaf4ad4dfa7414bd12293520dcdbe1a
navya1808 <navyagoyal400@gmail.com>
2022-08-25 16:10:46 +0530

    faq

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -23,12 +23,13 @@ export default function Faq() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography><h3>About Khelo India Scheme</h3></Typography>
+          <Typography><h3>What is E-Procurement System?</h3></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.</i>
+            <i>E-Procurement System is a process of procuring the items electronically using internet. 
+              This facility drastically reduces the tendering cycle time and reduces most of the indirect
+               costs and enhances transparency in procurement.</i>
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -38,12 +39,13 @@ export default function Faq() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography><h3>About Khelo India Scheme</h3></Typography>
+          <Typography><h3>How can I fill the tenders?</h3></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.</i>
+            <i>First you need to sign in/sign up the portal. And then for filling a particular tender you need to fulfill its eligibility criteria. 
+              Then you can navigate to the tender button and then choose the particular tender from the list and then click on the Get Started button for
+               filling the tender.</i>
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -53,12 +55,11 @@ export default function Faq() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography><h3>About Khelo India Scheme</h3></Typography>
+          <Typography><h3>What is the role of Admin?</h3></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.</i>
+            <i>Admin is the System Manager who makes the end decision for the approval/rejection of the proposals.</i>
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -68,12 +69,11 @@ export default function Faq() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography><h3>About Khelo India Scheme</h3></Typography>
+          <Typography><h3>How can admin view the data in various forms?</h3></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.</i>
+            <i>Admin can view the data in three forms i.e. list view , comparison view and analytic view on the Admin Dashboard of the portal.</i>
           </Typography>
         </AccordionDetails>
       </Accordion>
